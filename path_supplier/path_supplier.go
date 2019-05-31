0acd53cd8eb6beedf482caeada69c65d748a7057
LAPTOP-QR8NNK6N\ja <daczosnek@gmail.com>
2019-05-31 21:06:11 +0200

    add deserialization, test skeleton, extract baseUrl to field

--- path_supplier/path_supplier.go
+++ path_supplier/path_supplier.go
@@ -1,13 +1,15 @@
 package path_supplier
 
 import (
+	"encoding/json"
 	"fmt"
 	m "github.com/harfmuf/geolocation-generator/model"
+	"io/ioutil"
 	"net/http"
 )
 
 // precision to around .0001 deg, which corresponds to max 10m
-const URL_FORMAT = "%s%s?point=%.4f,%.4f&point=%.4f,%.4f&vehicle=%s&key=%s"
+const URL_FORMAT = "%s%s?point=%.4f,%.4f&point=%.4f,%.4f&vehicle=%s&key=%s&points_encoded=false"
 const GRAPHHOPPER_API_URL_BASE = "https://graphhopper.com/api/1"
 const ROUTING_API_URL = "/route"
 
@@ -16,11 +18,24 @@ type PathSupplier interface {
 }
 
 type GraphHopperPathSuplier struct {
-	apiKey string
+	baseUrl string
+	apiKey  string
 }
 
-func New(apiKey string) *GraphHopperPathSuplier {
-	return &GraphHopperPathSuplier{apiKey: apiKey}
+type Points struct {
+	Coordinates [][]float64
+}
+
+type Path struct {
+	Points Points
+}
+
+type GraphHopperResponse struct {
+	Paths []Path
+}
+
+func New(baseUrl string, apiKey string) *GraphHopperPathSuplier {
+	return &GraphHopperPathSuplier{apiKey: apiKey, baseUrl: baseUrl}
 }
 
 func (g GraphHopperPathSuplier) FindPath(from m.Location, to m.Location, vehicle string) [][]m.Location {
@@ -31,11 +46,30 @@ func (g GraphHopperPathSuplier) FindPath(from m.Location, to m.Location, vehicle
 	if err != nil || res.StatusCode != 200 {
 		panic(fmt.Sprintf("Request failed: %d, %s", res.StatusCode, res.Body))
 	}
-	return decodeResponseToPath(res)
+	return g.DecodeResponseToPath(res)
 }
 
-func decodeResponseToPath(response *http.Response) [][]m.Location {
-	return nil
+func (g GraphHopperPathSuplier) DecodeResponseToPath(response *http.Response) [][]m.Location {
+	var bodyDecoded GraphHopperResponse
+	body, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	validateErr(err)
+	err = json.Unmarshal(body, &bodyDecoded)
+	result := make([][]m.Location, len(bodyDecoded.Paths))
+	for i, path := range bodyDecoded.Paths {
+		currResult := make([]m.Location, len(path.Points.Coordinates))
+		for j, point := range path.Points.Coordinates {
+			currResult[j] = m.Location{Latitude: point[0], Longitude: point[1]}
+		}
+		result[i] = currResult
+	}
+	return result
+}
+
+func validateErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (g GraphHopperPathSuplier) GetUrlFromLocations(from m.Location, to m.Location, vehicle string) string {
@@ -46,7 +80,7 @@ func (g GraphHopperPathSuplier) GetUrlFromLocations(from m.Location, to m.Locati
 	toLon := to.Longitude
 	return fmt.Sprintf(
 		URL_FORMAT,
-		GRAPHHOPPER_API_URL_BASE,
+		g.baseUrl,
 		ROUTING_API_URL,
 		fromLat,
 		fromLon,
