9bc45256ee7f245289d87429e3a6b77ac96a555d
LAPTOP-QR8NNK6N\ja <daczosnek@gmail.com>
2019-05-31 16:03:57 +0200

    move test to appropriate package

--- path_supplier/path_supplier_test.go
+++ path_supplier/path_supplier_test.go
@@ -1,15 +1,14 @@
-package test
+package path_supplier
 
 import (
 	"github.com/harfmuf/geolocation-generator/model"
-	"github.com/harfmuf/geolocation-generator/path_supplier"
 	"gotest.tools/assert"
 	"testing"
 )
 
 func TestUrlComposedCorrectly(t *testing.T) {
 	//key := os.Getenv("graphhopperKey")
-	g := path_supplier.New("fakeKey")
+	g := New("fakeKey")
 	from := model.Location{Latitude: 1.0, Longitude: 2.0}
 	to := model.Location{Latitude: 6.66, Longitude: 6.66}
 	vehicle := "car"
