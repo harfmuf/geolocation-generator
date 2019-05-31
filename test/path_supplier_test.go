0ac0475d5a5c5c967b2d7be419840cc08fbdd5c3
LAPTOP-QR8NNK6N\ja <daczosnek@gmail.com>
2019-05-31 15:59:05 +0200

    add url formatter with test

---
+++ test/path_supplier_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"github.com/harfmuf/geolocation-generator/model"
+	"github.com/harfmuf/geolocation-generator/path_supplier"
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestUrlComposedCorrectly(t *testing.T) {
+	//key := os.Getenv("graphhopperKey")
+	g := path_supplier.New("fakeKey")
+	from := model.Location{Latitude: 1.0, Longitude: 2.0}
+	to := model.Location{Latitude: 6.66, Longitude: 6.66}
+	vehicle := "car"
+	url := g.GetUrlFromLocations(from, to, vehicle)
+
+	assert.Equal(t, url, "https://graphhopper.com/api/1/route?point=1.0000,2.0000&point=6.6600,6.6600&vehicle=car&key=fakeKey")
+}
